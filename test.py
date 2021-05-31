778cf1daace463932bb9059c1520a0339327cd45
Aaris-Kazi <aariskazi@gmail.com>
2021-05-31 19:02:22 +0530

    small test for prediction

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pandas as pd
 import numpy as np
 data = pd.read_csv('covid_19_india.csv')
@@ -5,5 +6,13 @@ nlist = []
 states = data['State']
 # print(states)
 nlist = list(dict.fromkeys(states))
-print(nlist)
-print('\n', states.unique())
+# print(nlist)
+# print('\n', states.unique())
+x = data['Date'].min()
+y = data['Date'].max()
+print(x)
+print(y)
+x = datetime.strptime(x, '%Y-%m-%d')
+y = datetime.strptime(y, '%Y-%m-%d')
+delta = y - x
+print(delta.days)
