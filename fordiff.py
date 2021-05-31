59d5020796f7ec61ca5879505c6c24ade9ce8bad
Aaris-Kazi <aariskazi@gmail.com>
2021-05-31 21:12:33 +0530

    difference method

--- fordiff.py
+++ fordiff.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 import pandas as pd
-data = pd.read_csv('covid_19_india.csv')
-x = data['Date'].min()
-y = data['Date'].max()
-print(x)
-print(y)
-x = datetime.strptime(x, '%Y-%m-%d')
-y = datetime.strptime(y, '%Y-%m-%d')
-delta = y - x
-print(delta.days)
+def difference():
+    data = pd.read_csv('covid_19_india.csv')
+    x = data['Date'].min()
+    y = data['Date'].max()
+    print(x)
+    print(y)
+    x = datetime.strptime(x, '%Y-%m-%d')
+    y = datetime.strptime(y, '%Y-%m-%d')
+    delta = y - x
+    print(delta.days)
+    return int(delta.days), x, y
