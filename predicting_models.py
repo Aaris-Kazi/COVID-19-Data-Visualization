f62a26a114f70e7843286bee666ba6c25aeadaa8
Aaris-Kazi <aariskazi@gmail.com>
2021-06-01 15:43:59 +0530

    stage changes

--- predicting_models.py
+++ predicting_models.py
@@ -7,19 +7,18 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 def predicting():
     # names = input('Enter the State Name:\t')
-    names = 'Kerala'
+    names = 'Maharashtra'
     data = pd.read_csv('covid_19_india.csv')
     try:
         city = data[data.State == names]
         if len(city) ==0:
             print('Enter the Right State Of India!')
         else:
-            dif, x, y =difference()
+            dif, lx, ly =difference()
             days= []
             y= city['Confirmed']
             for i in range(0,len(y)):
                 days.append(i)
-            
             days = np.array(days)
             days = days.reshape(-1,1)
             conf = np.array(y)
@@ -27,11 +26,18 @@ def predicting():
             # print(conf)
             conf = conf.reshape(-1,1)
             reg = LinearRegression().fit(days, conf)
-            a = np.array([[476]])
+            a = np.array([[437],[438],[439],[500],[550],[650], [800]])
             b = reg.predict(a)
             print(b)
             plt.plot(days,conf)
             plt.scatter(a, b)
+            lx = str(lx.date())
+            ly = str(ly.date())
+            print(lx, ly)
+            plt.title('Covid data between '+lx+' and '+ly)
+            plt.legend(['Actual Number', 'Predicted Number'])
+            plt.xlabel('Dates')
+            plt.ylabel('Number of People')
             plt.grid(True)
             plt.show()
     except Exception as e:
